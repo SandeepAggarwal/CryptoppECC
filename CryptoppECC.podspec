fdc51a9ea431f9f075dbc4e9e4080f01ba621943
Sandeep Aggarwal <@smartSandeep1129@gmail.com>
2016-01-04 00:13:33 +0530

    builder.sh updated,podspec updated

--- CryptoppECC.podspec
+++ CryptoppECC.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "CryptoppECC"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "CryptoppECC-Encryption/Decryption using ECC(Elliptic curve cryptography)"
 
 
@@ -21,20 +21,21 @@ Pod::Spec.new do |s|
   s.author             = { "Sandeep Aggarwal" => "smartSandeep1129@gmail.com" }
   s.social_media_url   = "https://twitter.com/sandeepCool77"
   s.ios.deployment_target = "7.0"
-  s.osx.deployment_target = "10.8"
-  s.prepare_command= "sudo sh CryptoppECC/CryptoppLibrary/builder.sh"
+  s.osx.deployment_target = "10.10"
+  s.osx.xcconfig =
+  {
+   'OTHER_LDFLAGS' => '-lc++'
+  }
+  s.prepare_command=<<-CMD
+                        sh CryptoppECC/CryptoppLibrary/builder.sh
+                        CMD
 
-  s.source       = { :git => "https://github.com/SandeepAggarwal/CryptoppECC.git", :tag =>"0.0.1" }
+  s.source       = { :git => "https://github.com/SandeepAggarwal/CryptoppECC.git", :tag =>"0.0.2" }
   s.source_files  = "CryptoppECC/CryptoppLibrary/*.h", "CryptoppECC/CryptoppWrapper/*.{h,mm}"
   s.public_header_files = "CryptoppECC/CryptoppWrapper/*.h"
-  s.vendored_library ="CryptoppECC/CryptoppLibrary/bin/libcryptopp.a"
+  s.osx.vendored_library ="CryptoppECC/CryptoppLibrary/bin/macosx/libcryptopp.a"
+  s.ios.vendored_library ="CryptoppECC/CryptoppLibrary/bin/ios/libcryptopp.a"
   s.prefix_header_file ="CryptoppECC/cryptodemolib.pch"
-
-    s.osx.xcconfig =
-    {
-    'OTHER_LDFLAGS' => '-lc++'
-    }
-
 
   s.requires_arc = true
 
--- CryptoppECC/CryptoppLibrary/builder.sh
+++ CryptoppECC/CryptoppLibrary/builder.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+cd CryptoppECC/CryptoppLibrary
+
 XCODE_ROOT=`xcode-select -print-path`
+
+echo "making for iOS"
 ARCHS="x86_64 i386 armv7 arm64"
-SDK_VERSION=9.2
+SDK_VERSION=`xcrun --sdk iphoneos --show-sdk-version 2> /dev/null`
+MIN_SDK_VERSION=7.0
 
-echo "min sdk version support:"
-echo $SDK_VERSION
-echo "pwd is"
-echo $PWD
 
-APPEND_PATH="CryptoppECC/CryptoppLibrary"
+echo "sdk version is: "
+echo ${SDK_VERSION}
 
-STATIC_ARCHIVES=""
+IOS_STATIC_ARCHIVES=""
 for ARCH in ${ARCHS}
 do
     PLATFORM=""
@@ -30,35 +32,82 @@ do
     export RANLIB=${TOOLCHAIN_ROOT}ranlib
     export ARFLAGS="-static -o"
     export LDFLAGS="-arch ${ARCH} -isysroot ${SDK_ROOT}"
-    export BUILD_PATH="${APPEND_PATH}/BUILD_${ARCH}"
-    export CXXFLAGS="-x c++ -arch ${ARCH} -isysroot ${SDK_ROOT} -I${BUILD_PATH} -miphoneos-version-min=7.0 -mios-simulator-version-min=7.0"
-    mkdir -p ${BUILD_PATH}
+    export BUILD_PATH="IOS_BUILD_${ARCH}"
+    export CXXFLAGS="-x c++ -arch ${ARCH} -isysroot ${SDK_ROOT} -I${BUILD_PATH} -miphoneos-version-min=${MIN_SDK_VERSION} -mios-simulator-version-min=${MIN_SDK_VERSION}"
 
+    mkdir -pv ${BUILD_PATH}
     make -f Makefile
+    mv *.o ${BUILD_PATH}
+    mv *.d ${BUILD_PATH}
+    mv libcryptopp.a ${BUILD_PATH}
 
-    mv ${APPEND_PATH}/*.o ${BUILD_PATH}
-    mv ${APPEND_PATH}/*.d ${BUILD_PATH}
-    mv ${APPEND_PATH}/libcryptopp.a ${BUILD_PATH}
-
-    STATIC_ARCHIVES="${STATIC_ARCHIVES} ${BUILD_PATH}/libcryptopp.a"
+    IOS_STATIC_ARCHIVES="${IOS_STATIC_ARCHIVES} ${BUILD_PATH}/libcryptopp.a"
 
 done
 
 echo "Creating universal library..."
-mkdir -p bin
-lipo -create ${STATIC_ARCHIVES} -output ${APPEND_PATH}/bin/libcryptopp.a
+ mkdir -p bin/ios
+ lipo -create ${IOS_STATIC_ARCHIVES} -output bin/ios/libcryptopp.a
 
 echo "removing thin archs"
 for ARCH in ${ARCHS}
 do
 
-    directoryName="${APPEND_PATH}/BUILD_${ARCH}"
+    directoryName=IOS_BUILD_${ARCH}
     rm -rf ${directoryName}
 
 done
 
+
+############################################################
+echo "making for macOSX"
+ARCHS="x86_64 i386"
+SDK_VERSION=`xcrun --sdk macosx --show-sdk-version 2> /dev/null`
+MIN_SDK_VERSION=10.10
+PLATFORM="MacOSX"
+
+
+echo "sdk version is: "
+echo ${SDK_VERSION}
+
+
+MACOSX_STATIC_ARCHIVES=""
+for ARCH in ${ARCHS}
+do
+
+export DEV_ROOT="${XCODE_ROOT}/Platforms/${PLATFORM}.platform/Developer"
+export SDK_ROOT="${DEV_ROOT}/SDKs/${PLATFORM}${SDK_VERSION}.sdk"
+export TOOLCHAIN_ROOT="${XCODE_ROOT}/Toolchains/XcodeDefault.xctoolchain/usr/bin/"
+export CC="clang -arch $ARCH -fembed-bitcode"
+export CXX=clang++
+export AR=${TOOLCHAIN_ROOT}libtool
+export RANLIB=${TOOLCHAIN_ROOT}ranlib
+export ARFLAGS="-static -o"
+export LDFLAGS="-arch ${ARCH} -isysroot ${SDK_ROOT}"
+export BUILD_PATH="MACOSX_BUILD_${ARCH}"
+export CXXFLAGS="-x c++ -arch ${ARCH} -isysroot ${SDK_ROOT} -I${BUILD_PATH} -mmacosx-version-min=${MIN_SDK_VERSION}"
+
+mkdir -pv ${BUILD_PATH}
+make -f Makefile
+mv *.o ${BUILD_PATH}
+mv *.d ${BUILD_PATH}
+mv libcryptopp.a ${BUILD_PATH}
+
+MACOSX_STATIC_ARCHIVES="${MACOSX_STATIC_ARCHIVES} ${BUILD_PATH}/libcryptopp.a"
+
+done
+
+echo "Creating universal library..."
+mkdir -p bin/macosx
+lipo -create ${MACOSX_STATIC_ARCHIVES} -output bin/macosx/libcryptopp.a
+
+echo "removing thin archs"
+for ARCH in ${ARCHS}
+do
+
+directoryName=MACOSX_BUILD_${ARCH}
+rm -rf ${directoryName}
+
+done
+
 echo "Build done!"
-
-
-
-
